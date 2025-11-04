fe8150c72d04be857fbad1f2e117b5234bff5892
kavimailru <kavi@mail.ru>
2025-11-04 18:02:52 +0300

    First

--- src/main/java/com/Basic_Authentication/Basic_Authentication1/config/SpringSecurityConfig.java
+++ src/main/java/com/Basic_Authentication/Basic_Authentication1/config/SpringSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,9 +15,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SpringSecurityConfig {
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
@@ -30,18 +33,18 @@ public class SpringSecurityConfig {
      */
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize)->{
-                    authorize.anyRequest().authenticated();
-                }).httpBasic(Customizer.withDefaults());
-
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    UserDetailsService userDetailsService(){
 
         UserDetails user1= User.builder()
                 .username("user1")
